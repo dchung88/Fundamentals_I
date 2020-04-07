a0267714160a6d3d0f3ad8a2d1810248ac69c5ed
Denny Chung <dennychung@Dennys-MacBook-Pro.local>
2020-04-07 13:26:35 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Fundamentals_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0) 
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0 )
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0 )
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+            {
+                Console.WriteLine(i);
+            }
+            }
+        }
+    }
+}
